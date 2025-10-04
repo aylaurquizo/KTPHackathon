5b5cdf1dcb24c6ae5ee353684a48b49db0726518
aylaurquizo <urquizo.a@northeastern.edu>
2025-10-04 11:47:43 -0400

    lets do this!

--- ktphackathon/src/main.jsx
+++ ktphackathon/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AppWrapper from './App.jsx'
-import './index.css' // Make sure TailwindCSS styles are imported
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AppWrapper />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
